0d6fb5b27b2afb2461f6918d5e4705824fd8eaab
Enuch <enuchsan@gmail.com>
2022-03-20 10:27:14 -0300

    Renderização condicional

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 
+const buttonA = <button>First Button</button>
+const buttonB = <button>Second Button</button>
+
+const hasCustomer = false
+
 const App = () => {
+
+    const renderSee = () => (
+            <div>
+                Clique no botão abaixo para visualizar algo
+                {buttonA}
+            </div>
+        )
+
+    const renderAdd = () => (
+            <div>
+                Clique abaixo para cadastrar
+                {buttonB}
+            </div>
+        )
+        
     return (
         <div>
             <p>Digital Innovation One</p>
             <p>Enuch Santo</p>
+            {hasCustomer ? renderSee() : renderAdd()}
         </div>
     )
 }
